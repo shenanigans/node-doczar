9c38e5cc40b1b03bd52cab59f38a312c349f6616
Kevin "Schmidty" Smith <shenanigans@kaztl.com>
2014-10-03 02:50:14 -0700

    fixed multiple callback args, improved docs slightly

--- main.js
+++ main.js
@@ -11,22 +11,91 @@ var Doczar = function (options) {
     this.options = options;
     this.fileopsOutstanding = 0;
     this.context = new ComponentCache();
+    this.warnings = [];
+    this.errors = [];
+    this.queue = [];
 };
 
+/**     @member/Function addLibrary
+    @api
+
+@argument/String libname
+*/
 Doczar.prototype.addLibrary = function (libname) {
 
 };
 
+/**     @member/Function addFile
+    @api
+
+@argument/String fname
+*/
 Doczar.prototype.addFile = function (fname) {
 
 };
 
+/**     @member/Function addJSModule
+    @api
+
+@argument/String fname
+*/
 Doczar.prototype.addJSModule = function (fname) {
 
 };
 
-Doczar.prototype.writeFiles = function (directory, callback) {
+/**     @member/Function close
+    @api
+    Finalizes the documentation tree and writes the configured output to disc, unless a critical
+    finalization error has occured. A final error and warning report will be given to the callback
+    when all filesystem changes have finished.
+@argument/String directory
+@callback callback
+    @argument/Array errors
+    @argument/Array warnings
+*/
+Doczar.prototype.close = function (directory, callback) {
 
+};
+
+/**     @member/Function finalize
+    @development
+
+@callback callback
+    @argument/Array errors
+    @argument/Array warnings
+*/
+Doczar.prototype.finalize = function (callback) {
+
+};
+
+/**     @member/Function writeFiles
+    @development
+
+@argument/String directory
+@callback callback
+    @argument/Array errors
+    @argument/Array warnings
+*/
+Doczar.prototype.writeFiles = function (directory, callback) {
+    if (this.fileopsOutstanding) {
+        var self = this;
+        this.queue.push (function(){ self.writeFiles (directory, callback); });
+        return;
+    }
+
+    if (this.errors.length) {
+        var errs = this.errors;
+        return process.nextTick (function(){ callback (errs); });
+    }
+};
+
+/**     @member/Function clearQueue
+    @development
+*/
+Doczar.prototype.clearQueue = function(){
+    for (var i in this.queue)
+        this.queue[i]();
+    delete this.queue;
 };
 
 var DEFAULT_DOCZAR = new Doczar ({});
