4ecb1f58f4ba3a7a1e23307f9dc60ae498e5aa4f
Kevin "Schmidty" Smith <shenanigans@kaztl.com>
2015-05-05 14:40:24 -0700

    old updates

--- standardLibs/nodejs/url.js
+++ standardLibs/nodejs/url.js
@@ -4,8 +4,24 @@
     @remote `https://nodejs.org/api/url.html#url_url`
 @Function parse
     @remote `https://nodejs.org/api/url.html#url_url_parse_urlstr_parsequerystring_slashesdenotehost`
+    @returns/.URL
 @Function format
     @remote `https://nodejs.org/api/url.html#url_url_format_urlobj`
 @Function resolve
     @remote `https://nodejs.org/api/url.html#url_url_resolve_from_to`
+*/
+
+/**     @struct URL
+@member/String|null protocol
+@member/Boolean slashes
+@member/String|null auth
+@member/String|null host
+@member/String|null port
+@member/String|null hostname
+@member/String|null hash
+@member/String|null search
+@member/String|null query
+@member/String|null pathname
+@member/String|null path
+@member/String|null href
 */
