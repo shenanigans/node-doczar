f903e93ebd0864874f5b3fec848289206c2738f3
root <root@debian>
2015-05-05 21:58:07 +0000

    prep for version update

--- cli.js
+++ cli.js
@@ -20,7 +20,7 @@ var required = require ('required');
 var resolve = require ('resolve');
 var glob = require ('glob');
 require ('colors');
-var bunyan = require ('bunyan');
+// var bunyan = require ('bunyan');
 var Parser = require ('./lib/Parser');
 var ComponentCache = require ('./lib/ComponentCache');
 
