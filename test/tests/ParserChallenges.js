b1dd6b8e2cdf52b9fc5c6ea143184998beb52823
Kevin "Schmidty" Smith <shenanigans@kaztl.com>
2015-08-08 23:34:18 -0700

    do not, oh man do not ever, sanitize to a trailing period
    win10 no longer supports deleting files or directories with invalid
    names. You are stuck with them for ev er.

--- test/tests/ParserChallenges.js
+++ test/tests/ParserChallenges.js
@@ -3,11 +3,14 @@
     Just a few tricky names to make sure that parser changes don't break anything. Also to guide
     development of support for `` `very complex`.`backtick paths` `` which the first solution did not
     cover.
-@member/String `backtick name #9, #9, #9...`
+@member/class `backtick name #9, #9, #9...`
 @member/Function `backtick method #57`
 @property/String `escape \` challenge 1`
 @property/String `escape\ \` challenge 2`
 @property/String `escape\ \\\` challenge 3`
+*/
+/**     @member/String `backtick name #9, #9, #9...`.foobar
+    Strange directory issue on windows test.
 */
 /**     @argument/String #`backtick method #57`(`backtick argument!`
     This is an argument of the `` `backtick method #57` `` function.
